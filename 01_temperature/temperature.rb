161d05fd4b30bb3f890fe6cd09909f0745cb0595
raini35 <rainier.f.go@gmail.com>
2016-08-13 22:55:35 -0400

    New folder

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,13 @@
+#write your code here
+def ftoc fahrenheit
+	celsius = (fahrenheit - 32 ) * (5.0 / 9.0)
+	
+	celsius 
+end 
+
+def ctof celsius
+	
+	fahrenheit = celsius * 1.8 + 32 
+	
+	fahrenheit
+end 
